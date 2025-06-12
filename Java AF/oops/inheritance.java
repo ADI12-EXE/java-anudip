224b8854f74b9f6a3209b619a8a97cf471c2b9e9
Aditya Sultane <adityasultanepatil12@gmail.com>
2025-06-12 11:46:06 +0530

    Update inheritance.java

--- Java AF/oops/inheritance.java
+++ Java AF/oops/inheritance.java
@@ -1,13 +1,9 @@
 class Employee {
     int salary = 60000;
-
 }
-
 class Engineer extends Employee {
     int benefits = 10000;
-
 }
-
 class Faang {
     public static void main(String[] args) {
         Engineer E1 = new Engineer();
