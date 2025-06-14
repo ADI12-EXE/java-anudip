151be93497f719edc6a86f24011b23e3b7d405b7
Aditya Sultane <adityasultanepatil12@gmail.com>
2025-06-14 22:45:18 +0530

    Delete Java AF/importance directory

--- Java AF/importance/hello.java
+++
@@ -1,9 +0,0 @@
-public class hello {
-    public static void main(String[] args) {
-        
-        // you can write your code here 
-
-
-
-    }
-}
--- Java AF/importance/output.java
+++
@@ -1,8 +0,0 @@
-
-public class output {
-    public static void main(String[] args) {
-        System.out.println("Hey there ");   // we use "" for string
-        System.out.println(10);             // in java we don't use "" for integer 
-        System.out.println('a');            // '' for characters
-    }
-}
